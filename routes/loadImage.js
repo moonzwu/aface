d34b63f82b8bf6c283c0fc6ce2ba8fdde587f663
root <root@AY140510002458709102Z.(none)>
2014-06-19 06:37:54 +0800

    enable the scale image function

--- routes/loadImage.js
+++ routes/loadImage.js
@@ -1,18 +1,24 @@
 var express = require('express');
+var imageops = require('./imageOps.js');
 var router = express.Router();
 
-
-/* GET home page. */
+/* post home page. */
 router.post('/', function(req, res) {
     var width = req.body.browserClient.width;
     var hight = req.body.browserClient.hight;
     console.log(width);    
-    console.log(hight);    
-    res.render('loadImage', {
-        title: 'My Face',
-        clientWidth : width,
-        clientHight : hight
-    });
+    console.log(hight);
+
+    var renderPage = function(imgInfo) {
+        console.log(imgInfo);
+        res.render('loadImage', {
+            title: 'My Face',
+            imageName: imgInfo.imageName,
+            clientWidth : imgInfo.clientWidth,
+            clientHight : imgInfo.clientHight
+        });}
+
+    imageops.loadImage({'width': width, 'hight': hight}, '/root/nodejs_project/aface/public/images/flower.jpg', renderPage);
 });
 
 module.exports = router;
