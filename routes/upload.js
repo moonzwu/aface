44d2620052e951c6a4ca7a6e95a32e6dfd3ab4fa
root <root@AY140510002458709102Z.(none)>
2014-07-25 06:55:55 +0800

    enable upload image function

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
+var formidable = require('formidable');
+var fs = require('fs');
+var path = require('path');
 
 /* Deal with upload post*/
 router.post('/', function(req, res) {
     console.log("Receive the submit image request");
-    console.log(req.body);
-    console.log(req.files);
+    
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var oldPath = files.imageFile.path,
+            fileSize = files.imageFile.size,
+            fileExt = files.imageFile.name.split('.').pop(),
+            index = oldPath.lastIndexOf('/') + 1,
+            fileName = oldPath.substr(index),
+            originalFileName = files.imageFile.name,
+            newPath = path.join(process.env.PWD, '/uploads/' + fileName);
+    
+        console.log('original file name = ' + originalFileName);
+        console.log('file path = ' + oldPath + ', size=' + fileSize);
+        console.log('file name = ' + fileName);
+        console.log('new file path = ' + newPath);
+    });
+
 });
 
 router.get('/', function(req, res) {
